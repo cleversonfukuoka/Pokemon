439ab206c95bd447e1d38af818a88634c16dc9af
cleversonfukuoka <cleverson.fukuoka@hotmail.com>
2025-05-05 20:56:00 -0300

    Initial Commit

---
+++ Pokemon/PokemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonApp.swift
+//  Pokemon
+//
+//  Created by Cleverson Fukuoka on 05/05/25.
+//
+
+import SwiftUI
+
+@main
+struct PokemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
